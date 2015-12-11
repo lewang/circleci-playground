e755d4bc970329836de6c917919f8efd61cdd92f
Kim, Hirokuni <yangkookkim@gmail.com>
2015-12-11 11:41:54 +0900

    wip

--- wait-for-cassandra.sh
+++ wait-for-cassandra.sh
@@ -2,11 +2,10 @@
 echo "describe cluster;" > /tmp/dc
 for i in {1..5}; do
   echo Attempt $i
-  if cassandra-cli -f /tmp/dc 2>&1 | grep "Cluster Information"; then
+  if nodetool status | grep "^UN"; then
     exit 0
   else
     sleep 10
   fi
 done
 exit 1
-
